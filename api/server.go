676b25afdfbc6fc99042516d920d9acf280f269b
AnuchitO <anuchit.prasertsang@gmail.com>
2022-11-26 16:07:20 +0700

    server

--- api/server.go
+++ api/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -13,23 +15,44 @@ import (
     "status": "active"
   }
 */
+type todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
 
-var id int
-var title string
-var status string
+var t1 = todo{ID: 1, Title: "buy new ipad", Status: "active"}
+var t2 = todo{ID: 2, Title: "hello new ipad", Status: "active"}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		body := []byte(s)
+var todos = []todo{t1, t2}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		body, err := json.Marshal(todos)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(body)
 		return
 	}
 
-	if r.Method == "POST" {
-		body := []byte(`{"message": "AnuchitO"}`)
+	if req.Method == "POST" {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		var t todo
+		err = json.Unmarshal(body, &t)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		todos = append(todos, t)
 		w.WriteHeader(http.StatusCreated)
-		w.Write(body)
 		return
 	}
 
@@ -38,9 +61,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fmt.Println("start..")
-	http.HandleFunc("/hello", handler)
+	http.HandleFunc("/todos", handler)
 	fmt.Println("server..")
-	err := http.ListenAndServe("localhost:2565", nil)
+	err := http.ListenAndServe("localhost:1323", nil)
 	if err != nil {
 		fmt.Println("error..", err)
 		return
